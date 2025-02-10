f3383561f0964893f1785fa21f6931c57f684e58
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-02-11 00:42:51 +0600

    User login Details store in rtdb functionality added

--- script/login.js
+++ script/login.js
@@ -1,6 +1,8 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth, signInWithPopup, OAuthProvider } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
- 
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyC51WaB_HGCmQGABrEFxc3hWBdkUEVnkyI",
     authDomain: "app1-ed7d8.firebaseapp.com",
@@ -15,34 +17,53 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new OAuthProvider('microsoft.com');
-const connected='false';
+const database=getDatabase(app);
+const user=auth.currentUser;//ony set currentUser after auth
+//const connected='false';
 
 const login = document.getElementById("microsoft-login");
 const status = document.getElementById("status");
 
+const messageRefRakibLogin=ref(database,'LoginInfo');
+
 login.addEventListener('click', (event) => {
     signInWithPopup(auth, provider)
         .then((result) => {
+            const userRakib = result.user; // This will now contain the user info
+
             // After signed in.
-            const credential = OAuthProvider.credentialFromResult(result);
-            const accessToken = credential.accessToken;
-            const idToken = credential.idToken;
-           //const uid=user.result.uid
-          // console.log(uid);
+        //     const credential = OAuthProvider.credentialFromResult(result);
+        //     const accessToken = credential.accessToken;
+        //     const idToken = credential.idToken;
+        //    const uid=user.result.uid
+        //   console.log(uid);
             console.log("Successfully logged in");
             console.log("User Info:", result.user);
             console.log("UID",result.user.uid);
             const r=console.log("ID",result.user.email.substring(0,10));
             status.textContent = `Welcome, ${result.user.displayName}`; 
-            window.location.href='../html/chat_page.html';
             // connected='true';
-        })
+//User details saved in db
+
+const rakibAccData={
+userName:userRakib.displayName,
+email:userRakib.email,
+};
+
+ 
+ push(messageRefRakibLogin, rakibAccData)
+ .catch((error) => console.error("Error storing login details:", error));
+
+ window.location.href='../html/chat_page.html';
+})
         .catch((error) => {
           
             const errorMessage = error.message || "An unknown error occurred.";
             status.textContent = `Login failed. Error: ${errorMessage}`; 
         });
 });
+
+
 
 // if(connected=='true'){
 //     window.location.href='../html/chat_page.html';
